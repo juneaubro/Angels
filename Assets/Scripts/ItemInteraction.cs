a139e897b3c4efeb6bef80fbe42f39cde45c9458
juneau <juneaugawat@hotmail.com>
2020-10-21 16:05:58 -0400

    First puzzle room done.

---
+++ Assets/Scripts/ItemInteraction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteraction : MonoBehaviour
+{
+    public GameObject block;
+    public GameObject tnt1;
+    public GameObject tnt2;
+    public GameObject tnt3;
+    public GameObject tnt4;
+
+    public Animator tntExplode1;
+    public Animator tntExplode2;
+    public Animator tntExplode3;
+    public Animator tntExplode4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "tntTrigger")
+        {
+            try
+            {
+                tntExplode1.SetBool("explode", true);
+                tntExplode2.SetBool("explode", true);
+
+                Destroy(tnt1, 3);
+                Destroy(block, 2.7f);
+                Destroy(tnt2, 3);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+        if(col.name == "DoorCloseTrigger")
+        {
+            try
+            {
+                tnt3.SetActive(true);
+                tnt4.SetActive(true);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+        if(col.name == "pressure plate")
+        {
+            try
+            {
+                tntExplode3.SetBool("explode", true);
+                tntExplode4.SetBool("explode", true);
+
+                Destroy(tnt3, 3);
+                Destroy(tnt4, 3);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+}
